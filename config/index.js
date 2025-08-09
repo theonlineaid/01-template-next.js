587c841c5c1486df10e48810bc0edaa4469548d6
Omor Faruk <46064292+theonlineaid@users.noreply.github.com>
2025-08-09 22:45:48 +0600

    add domain

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
     ? "http://localhost:3000"
-    : "https://aid-nextjs.vercel.app";
+    : "https://aidmart.vercel.app";
